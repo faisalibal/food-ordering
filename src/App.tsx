36261b6c5b6c79dfc8495afe747414a8feadfc28
faisalibal33 <faisaliqbal030397@gmail.com>
2023-02-01 16:30:37 +0700

    [add]new app

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { useEffect } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AccountInfo from "./page/account-info/account-info";
-import { AccountPage } from "./page/account-page/AccountPage";
-import { AddReservationPage } from "./page/add-reservation-page/AddReservationPage";
-import { Allitems } from "./page/all-items/Allitems";
-import { EditProfile } from "./page/edit-profile/EditProfile";
-import { Favourites } from "./page/favourites-page/Favourites";
-import { GuestLoginPage } from "./page/guest-login-page/GuestLoginPage";
-import { HomePage } from "./page/home-page/HomePage";
-import { LandingPage } from "./page/landing-page/LandingPage";
-import { OrderList } from "./page/order-list/OrderList";
-import { Reservation } from "./page/reservation/Reservation";
-import { fetchFavourite } from "./redux/favouriteSlice";
-import { useAppDispatch } from "./redux/hook";
-import { fetchOrderList } from "./redux/OrderListSlice";
+import { useEffect } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AccountInfo from './page/account-info/account-info';
+import { AccountPage } from './page/account-page/AccountPage';
+import { AddReservationPage } from './page/add-reservation-page/AddReservationPage';
+import { Allitems } from './page/all-items/Allitems';
+import { EditProfile } from './page/edit-profile/EditProfile';
+import { Favourites } from './page/favourites-page/Favourites';
+import { GuestLoginPage } from './page/guest-login-page/GuestLoginPage';
+import { HomePage } from './page/home-page/HomePage';
+import { LandingPage } from './page/landing-page/LandingPage';
+import { OrderList } from './page/order-list/OrderList';
+import { Reservation } from './page/reservation/Reservation';
+import { fetchFavourite } from './redux/favouriteSlice';
+import { useAppDispatch } from './redux/hook';
+import { fetchOrderList } from './redux/OrderListSlice';
 
 function App() {
   const dispatch = useAppDispatch();
